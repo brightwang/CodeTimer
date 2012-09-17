8fc4b7fde256f2f4eec9627915a22f450cc9f67b
brightwang <brightwang1984@gmail.com>
2012-09-17 13:22:27 +0800

    源码

---
+++ CodeTimerTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CodeTimer;
+using System.Threading;
+
+namespace CodeTimerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CodeTimer.CodeTimer.Time("demo", 1000, () => 
+            //{
+            //    GC.Collect();
+            //});
+
+            int i = 1, j = 10;
+            do
+            {
+                if (i++ > --j)
+                    continue;
+            } while (i < 5);
+            Console.WriteLine(string.Format("{0} {1}",i,j));
+
+            Console.Read();
+        }
+    }
+}
